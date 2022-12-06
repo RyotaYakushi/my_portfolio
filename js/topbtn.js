b9c6087017ad93064fd4337922438abe5b6b97f9
ryota <janner.ryota@icloud.com>
2022-12-06 21:35:43 +0900

    css追加　html追加

---
+++ js/topbtn.js
@@ -0,0 +1,14 @@
+// スクロールで表示;
+(() => {
+  const topBtn = document.querySelector(".top-btn");
+  window.addEventListener("scroll", () => {
+    let scrollY = window.scrollY;
+    if (scrollY >= 500) {
+      //classにactive付与
+      topBtn.classList.add("active");
+    } else {
+      //classからactive削除
+      topBtn.classList.remove("active");
+    }
+  });
+})();
