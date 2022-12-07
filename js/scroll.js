fa58aea094f5353ecd9aadaf19e13e8b51532f08
ryota <janner.ryota@icloud.com>
2022-12-08 01:10:21 +0900

    スクロールでヘッダー固定追加

--- js/scroll.js
+++ js/scroll.js
@@ -1,14 +1,25 @@
 //  スクロール表示
 (() => {
-  const bottomBtn = document.querySelector("");
+  const fixde = document.querySelector(".header-top");
+  const show = document.querySelector(".logo_white");
+  const hidden = document.querySelector(".logo");
   window.addEventListener("scroll", () => {
     let scrollY = window.scrollY;
-    if (scrollY >= 70) {
+    if (scrollY >= 500) {
       //classにactive付与
-      .classList.add("active");
+      fixde.classList.add("is-active");
     } else {
       //classからactive削除
-      .classList.remove("active");
+      fixde.classList.remove("is-active");
+    }
+    if (scrollY >= 500) {
+      //classにshow.hidde付与
+      show.classList.add("is-show");
+      hidden.classList.add("is-hidden");
+    } else {
+      //classからshow.hidden削除
+      show.classList.remove("is-show");
+      hidden.classList.remove("is-hidden");
     }
   });
 })();
